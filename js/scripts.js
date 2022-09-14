7148213d11d9a2393f144f3e39ade59b0b9a7020
ALichrieff <alichrief12gmail.com>
2022-09-14 11:30:53 +0300

    2nd method to fetch the data into an API into url

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ myForm.addEventListener('submit',function(e){
     e.preventDefault() //prevent the page form reloading or navigating when submit the form 
     const formData =new FormData(this)// collection of key value pairs of the form it self, store data inside formData object 
    // set the content of the request to the top 
-    fetch('send.php', {
+    fetch('http://localhost/assignment/startbootstrap-freelancer-gh-pages/contact-retrieve-data/send.php', {
         method: 'post',
         body:formData
     })
@@ -13,3 +13,30 @@ myForm.addEventListener('submit',function(e){
     })
 
 })
+
+/*
+
+
+ another way 
+
+
+const myForm= document.getElementById("contactForm")
+const fullname= document.getElementById("name").value
+const email = document.getElementById("email").value
+const phone = document.getElementById("phone").value
+const message = document.getElementById("message").value
+
+myForm.addEventListener('submit',sendData)
+
+function sendData(e){
+    e.preventDefault() //prevent the page form reloading or navigating when submit the form 
+    var searchparams = new URLSearchParams({"fullname":fullname,"email":email,"phone":phone,"message":message})
+
+    fetch('http://localhost/assignment/startbootstrap-freelancer-gh-pages/contact-retrieve-data/send.php', {
+        method: 'POST',
+        // body:formData
+        body:searchparams
+    })
+}
+
+*/
